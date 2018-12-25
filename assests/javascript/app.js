8a2bb6ae1f87ff49d6f5b0be1e3a501dd1a9e40f
Silpa <silpashinto@gmail.com>
2018-12-25 18:19:09 -0500

    bonus

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -16,20 +16,24 @@ firebase.initializeApp(config);
 //get reference to the Db
 var database = firebase.database();
 
-
 // Calls storeInputs function if submit button clicked
 $("#addtrain").on("click", function (event) {
+
+    validate();
+    storeInDb(event);
+
+});
+
+
+function validate() {
 
     // form validation - if empty - alert
     if ($('#trainName').val().length === 0 || $('#destination').val().length === 0 || $("#firstTrainTime").length === 0 || $('#frequency') === 0) {
         alert("Please Fill All Required Fields");
         event.preventDefault();
-
-    } else {
-        // if form is filled out, run function
-        storeInDb(event);
     }
-});
+
+}
 
 // Calls storeInputs function if enter key is clicked
 $('#trainForm').on("keypress", function (event) {
@@ -46,6 +50,7 @@ $('#trainForm').on("keypress", function (event) {
         }
     }
 });
+
 
 //On add button click       
 var storeInDb = function (event) {
@@ -82,7 +87,7 @@ database.ref().orderByChild("dateAdded").on("child_added", function (childSnapsh
     var trainName = childSnapshot.val().trainName;
     var trainDestination = childSnapshot.val().destination;
     var trainTime = childSnapshot.val().firstTrainTime;
-    var trainFrequency = childSnapshot.val().frequency;   
+    var trainFrequency = childSnapshot.val().frequency;
 
     //Declaring a time difference variable
     var timeDifference = moment().diff(moment.unix(trainTime), "minutes");
@@ -100,18 +105,95 @@ database.ref().orderByChild("dateAdded").on("child_added", function (childSnapsh
     var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm A");
     console.log('next arraival' + nextArrival);
 
+    createTable(trainName, trainDestination, trainFrequency, nextArrival, minutesAway, childSnapshot.key);
+
+
+
+
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
+
+function createTable(trainName, trainDestination, trainFrequency, nextArrival, minutesAway, key) {
+
     //html table row element
     var tr = $('<tr>').append(
         $('<td>').text(trainName),
         $('<td>').text(trainDestination),
         $('<td>').text(trainFrequency),
         $('<td>').text(nextArrival),
-        $('<td>').text(minutesAway)
+        $('<td>').text(minutesAway),
+        $('<td>').html("<a href='#' class ='editTrainRecord' data-name ='" + key + "'><i class='fa fa-pencil-square' aria-hidden='true'></i></a>"),
+        $('<td>').html("<a href='#' data-name ='" + key + "' class ='deleteRecord'><i class='fa fa-trash' aria-hidden='true'></i></a>")
     );
+
     $('.tbody').append(tr);
 
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
+    $(".editTrainRecord").on("click", function (event) {
+        event.preventDefault();
+
+        var recordId = $(this).attr('data-name');
+        database.ref().child(recordId).on("value", function (snapshot) {
+
+            console.log(snapshot.val()); // here's your data object
+
+            $('#trainName').val(snapshot.val().trainName);
+            $('#destination').val(snapshot.val().destination);
+            $('#frequency').val(snapshot.val().frequency);
+            $("#firstTrainTime").val(moment.unix(snapshot.val().firstTrainTime).format("hh:mm"));
+
+        });
+        $('#addtrain').hide();
+        $('#updatetrain').show();
+        $('#updatetrain').attr('data-name', recordId);
+
+    });
+
+    $(".deleteRecord").on("click", function () {
+        var recordId = $(this).attr('data-name');
+        var reply = confirm('Do you realy want to remove the train details?');
+        if(reply)database.ref().child(recordId).remove();
+    });
+
+
+
+}
+
+$("#updatetrain").on("click", function () {
+
+    validate();
+    var recordId = $(this).attr('data-name');
+    var recodUpdate = updateInDb(recordId);
+
 });
+
+var updateInDb = function (recordId) {
+
+
+    //Getting all the inputs
+    var trainName = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var frequency = $('#frequency').val().trim();
+    var firstTrainTime = $("#firstTrainTime").val().trim();
+    var e_trainTime = moment(firstTrainTime, "HH:mm").format("X");
+
+    //push to DB
+    postData = {
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency,
+        firstTrainTime: e_trainTime,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
+    //clear the fields
+    $('#trainName').val('');
+    $('#destination').val('');
+    $('#frequency').val('');
+    $("#firstTrainTime").val('');
+
+    return database.ref().child(recordId).update(postData);
+}
+
+
 
 
