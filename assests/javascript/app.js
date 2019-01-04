6f57f32feda70b2a921be1f8a87c6e10cc703ed9
Silpa <silpashinto@gmail.com>
2019-01-04 12:11:18 -0500

    test

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -12,6 +12,9 @@ var config = {
     storageBucket: "trains-386db.appspot.com",
     messagingSenderId: "971363359981"
 };
+
+    
+
 firebase.initializeApp(config);
 //get reference to the Db
 var database = firebase.database();
@@ -195,5 +198,36 @@ var updateInDb = function (recordId) {
 }
 
 
+    
 
 
+var provider = new firebase.auth.GoogleAuthProvider();
+       
+
+function googleSignin() {
+   firebase.auth()
+   
+   .signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+		
+      console.log(error.code)
+      console.log(error.message)
+   });
+}
+
+function googleSignout() {
+   firebase.auth().signOut()
+	
+   .then(function() {
+      console.log('Signout Succesfull')
+   }, function(error) {
+      console.log('Signout Failed')  
+   });
+}
